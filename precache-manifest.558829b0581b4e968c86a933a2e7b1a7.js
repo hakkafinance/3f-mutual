576678f22074f2422e2804fc6edfc7f37b134be3
hakka-finance <62842993+hakka-finance@users.noreply.github.com>
2020-05-12 18:44:24 +0800

    Updates

---
+++ precache-manifest.558829b0581b4e968c86a933a2e7b1a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfdbbdeb44f003e47b5b0455bf95d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010bdf94330e3dab0f17",
+    "url": "/static/js/2.1b7e768a.chunk.js"
+  },
+  {
+    "revision": "cb7cb099aa42271ba4ec78d15fd0a0d9",
+    "url": "/static/js/2.1b7e768a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec481c8d5f13a4d05d91",
+    "url": "/static/js/main.96ce181a.chunk.js"
+  },
+  {
+    "revision": "75765fa82858050a3813",
+    "url": "/static/js/runtime-main.e9d132f9.js"
+  },
+  {
+    "revision": "d155fef47ab9367a7c1f1fe57b845cb9",
+    "url": "/static/media/background.d155fef4.svg"
+  },
+  {
+    "revision": "bfd03a74d3d13f359e04aeb0b351d487",
+    "url": "/static/media/clock.bfd03a74.svg"
+  },
+  {
+    "revision": "9b9cb11e0c7b2b4127d027d2d5ec2d24",
+    "url": "/static/media/copy.9b9cb11e.svg"
+  },
+  {
+    "revision": "936922af8aebbbc8bfe05f0af12c83ba",
+    "url": "/static/media/ethereum.936922af.svg"
+  },
+  {
+    "revision": "02fad98e6f0929801ed00cab3a0d0b0b",
+    "url": "/static/media/jumbotron-background.02fad98e.svg"
+  },
+  {
+    "revision": "8edab182205847a360d8fa638d49ee5d",
+    "url": "/static/media/logo.8edab182.svg"
+  }
+]);
